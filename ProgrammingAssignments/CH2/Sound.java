5967ecb495fdc762f2a139a15498939581a70db9
Andrew Picard <andrewpicard@Andrews-MacBook-Pro.local>
2016-07-01 15:13:26 -0700

    Started Chapter 2

---
+++ ProgrammingAssignments/CH2/Sound.java
@@ -0,0 +1,7 @@
+class Sound{
+	public static void main(String args[]){
+		double soundTravel = 1100, timeInterval = 7.2, distance;
+		distance = soundTravel * timeInterval;
+		System.out.println("The lightning is " + distance + " feet away.");
+	}
+}

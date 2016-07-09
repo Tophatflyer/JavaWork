25c8bf9fcfbd44e9ed68e3277f7580c4fd5fa78e
Andrew Picard <andrewpicard@andrews-mbp.attlocal.net>
2016-07-09 03:48:41 -0700

    learning how commit works

--- Test.java
+++ Test.java
@@ -1,7 +1,9 @@
 class Test{
 	public static void main(String args[]){
-		String temp = "I like Java";
-		System.out.println("The length of the string \"I like Java\" is: " + temp.length() + " and the character at index 0 is: " + temp.charAt(0));
-		int array[] = {1,2,3,4,5};
+		int sum = 0;
+		for(int i = 0; i < 10; ++i){
+			sum = sum + i;
+		}
+		System.out.println("sum from 0 to 9 is: " + sum);
 	}
 }
